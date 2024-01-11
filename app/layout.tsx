da5ac863bdcd9c8ba0c43501c265533ba4c7a814
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-11 22:41:12 +0700

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./ui/navbar";
 import Footer from "./ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,8 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="relative">
       <body className={inter.className}>
+        <Navbar />
         <main>{children}</main>
         <Footer />
       </body>
