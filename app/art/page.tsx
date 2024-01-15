c0327213c3275fce5cd4b44e4c3260e10f6fd82e
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-15 19:48:02 +0700

    init art page

---
+++ app/art/page.tsx
@@ -0,0 +1,10 @@
+export default function Art() {
+  return (
+    <div>
+      <header>
+        <h1>Art</h1>
+        <p>This page is for my recent art, hope you enjoy it!</p>
+      </header>
+    </div>
+  );
+}
