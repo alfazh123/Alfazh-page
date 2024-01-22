18bf74d47a74057be353de1f9bbfa7f438d163ce
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-22 18:27:52 +0700

    feat: project card skeleton

--- app/ui/project-card.tsx
+++ app/ui/project-card.tsx
@@ -44,24 +44,6 @@ export default function ProjectCard({
     );
 }
 
-export function ProjectCardSkeleton() {
-    return (
-        <div className="flex flex-col bg-slate-800 text-slate-200 shadow-2xl transition ease-in-out delay-150 hover:border-solid hover:border-2 hover:scale-105 hover:border-slate-200 duration-300 w-full rounded-lg">
-            <div className="m-2">
-                <div className="w-full aspect-auto rounded-md bg-slate-700 animate-pulse">
-                    <div className="h-96 w-full rounded-md" />
-                </div>
-                <div className="items-center mx-4">
-                    <div className="text-lg font-bold my-3 mx-1 bg-slate-700 animate-pulse h-8 rounded-md" />
-                    <div className="justify-between mb-4">
-                        <div className="bg-slate-700 animate-pulse h-6 rounded-md w-1/4" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
 export function CardProject({
     title,
     description,
