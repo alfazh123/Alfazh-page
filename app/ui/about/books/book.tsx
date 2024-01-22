e55661ee07cd3311ceda18aeffa34c7724343c22
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-22 18:25:58 +0700

    fix: format data

--- app/ui/about/books/book.tsx
+++ app/ui/about/books/book.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 
 interface BookProps {
-    id: number;
+    id: string;
     title: string;
     author?: string[];
     description: string;
