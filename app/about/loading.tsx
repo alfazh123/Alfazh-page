cab352c24589e7f9413fc7c1cd45c3a2ec144a5b
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-22 18:25:11 +0700

    init loading about and project page

---
+++ app/about/loading.tsx
@@ -0,0 +1,13 @@
+import { ProjectCardSkeleton } from "../ui/skeletonCards";
+
+export default function Project() {
+    return (
+        <div className="min-h-screen flex flex-col space-y-20 px-4">
+            <div className="text-lg font-bold my-3 mx-1 bg-slate-700 animate-pulse h-20 rounded-md mb-4"></div>
+            <div className="flex flex-col space-y-4">
+                <ProjectCardSkeleton />
+                <ProjectCardSkeleton />
+            </div>
+        </div>
+    );
+}
