03aa997e576d0c3efd36dcd1f9859ac4995587e4
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-30 21:53:20 +0700

    fix: move data to db.json

--- app/art/page.tsx
+++ app/art/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import db from "@/app/lib/db.json";
 
 export default function Art() {
     return (
@@ -8,56 +9,17 @@ export default function Art() {
                 <p>This page is for my recent art, hope you enjoy it!</p>
             </header>
 
-            <div className="columns-4 gap-4 space-y-4">
-                <Image
-                    src="/art1.png"
-                    alt="1"
-                    width={500}
-                    height={500}
-                    className="w-full h-full aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
-                <Image
-                    src="/art2.png"
-                    alt="2"
-                    width={500}
-                    height={500}
-                    className="w-full h-full aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
-                <Image
-                    src="/art/3.jpg"
-                    alt="3"
-                    width={500}
-                    height={500}
-                    className="w-full h-80 aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
-                <Image
-                    src="/art/4.jpg"
-                    alt="4"
-                    width={500}
-                    height={500}
-                    className="w-full h-52 aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
-                <Image
-                    src="/art/5.jpg"
-                    alt="5"
-                    width={500}
-                    height={500}
-                    className="w-full h-96 aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
-                <Image
-                    src="/art/6.jpg"
-                    alt="6"
-                    width={500}
-                    height={500}
-                    className="w-full h-32 aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
-                <Image
-                    src="/art/7.jpg"
-                    alt="7"
-                    width={500}
-                    height={500}
-                    className="w-full h-96 aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
-                />
+            <div className="md:columns-4 columns-2 gap-4 space-y-4">
+                {db.art.map((art, id) => (
+                    <Image
+                        key={id}
+                        src={art["image-url"]}
+                        alt={art.title}
+                        width={500}
+                        height={500}
+                        className="w-full h-full aspect-auto rounded-lg bg-slate-500 break-inside-avoid hover:scale-105 transition ease-in-out duration-300"
+                    />
+                ))}
             </div>
         </div>
     );
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,33 +1,18 @@
-import { CardProject } from "../ui/project-card";
-
-const projects = [
-    {
-        title: "ACE 2024",
-        description: "NextJS is a React Framework for production",
-        src: "/ace2024.png",
-        link: "https://ace2024.vercel.app",
-        tag: ["NextJS", "Tailwind", "MDX", "TS"],
-    },
-    {
-        title: "Redesign KediriKab",
-        description: "Tailwind is a utility-first CSS framework",
-        src: "/next.svg",
-        link: "",
-    },
-];
+import ProjectCard from "../ui/project-card";
+import db from "@/app/lib/db.json";
 
 export default function Project() {
     return (
         <div className="min-h-screen flex flex-col space-y-20 px-4">
             <h1 className="font-bold text-6xl">Project</h1>
-            <div className="flex flex-col space-y-4">
-                {projects.map((project, index) => (
-                    <div key={index}>
-                        <CardProject
+            <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4">
+                {db.projects.map((project, index) => (
+                    <div key={index} className="snap-center shrink-0">
+                        <ProjectCard
                             title={project.title}
                             description={project.description}
                             src={project.src}
-                            href={project.link}
+                            href={project.href}
                             tag={project.tag}
                         />
                     </div>
