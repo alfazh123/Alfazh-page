353f63cbf484e24319cbd6d2d1ec42e5563b0ce0
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-02-03 21:53:40 +0700

    feat: dark theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,3 +27,32 @@ const config: Config = {
     plugins: [],
 };
 export default config;
+
+module.exports = {
+    mode: "jit",
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            animation: {
+                "infinite-scroll": "infinite-scroll 10s linear infinite",
+            },
+            keyframes: {
+                "infinite-scroll": {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-100%)" },
+                },
+            },
+        },
+    },
+    plugins: [],
+};
