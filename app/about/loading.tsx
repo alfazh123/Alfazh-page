15839a53886dfa7f79819545ddee6c92e93d0f64
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-02-03 20:23:24 +0700

    fix: loading when light mode

--- app/about/loading.tsx
+++ app/about/loading.tsx
@@ -1,12 +1,16 @@
-import { ProjectCardSkeleton } from "../ui/skeletonCards";
-
 export default function Project() {
     return (
         <div className="min-h-screen flex flex-col space-y-20 px-4">
-            <div className="text-lg font-bold my-3 mx-1 bg-slate-700 animate-pulse h-20 rounded-md mb-4"></div>
-            <div className="flex flex-col space-y-4">
-                <ProjectCardSkeleton />
-                <ProjectCardSkeleton />
+            <div className="text-lg font-bold my-3 mx-1 dark:bg-slate-700 bg-slate-300 w-96 animate-pulse h-20 rounded-md mb-4"></div>
+            <div className="text-lg font-bold my-3 mx-1 dark:bg-slate-700 bg-slate-300 w-full animate-pulse h-20 rounded-md mb-4">
+                <div className="w-full aspect-auto rounded-md dark:bg-slate-700 bg-slate-400 animate-pulse">
+                    <div className="h-40 w-full rounded-md" />
+                </div>
+                <div className="w-full h-8 dark:bg-slate-700 bg-slate-400 animate-pulse"></div>
+                <div className="w-full h-8 dark:bg-slate-700 bg-slate-400 animate-pulse"></div>
+                <div className="w-full h-8 dark:bg-slate-700 bg-slate-400 animate-pulse"></div>
+                <div className="w-full h-8 dark:bg-slate-700 bg-slate-400 animate-pulse"></div>
+                <div className="w-full h-8 dark:bg-slate-700 bg-slate-400 animate-pulse"></div>
             </div>
         </div>
     );
--- app/project/loading.tsx
+++ app/project/loading.tsx
@@ -1,12 +1,13 @@
-import { ProjectCardSkeleton } from "../ui/skeletonCards";
+import { SkeletonCardLinkProject } from "../ui/skeletonCards";
 
 export default function Project() {
     return (
         <div className="min-h-screen flex flex-col space-y-20 px-4">
-            <div className="text-lg font-bold my-3 mx-1 bg-slate-700 animate-pulse h-20 rounded-md mb-4"></div>
-            <div className="flex flex-col space-y-4">
-                <ProjectCardSkeleton />
-                <ProjectCardSkeleton />
+            <div className="text-lg font-bold my-3 mx-1 dark:bg-slate-700 bg-slate-300 animate-pulse h-20 w-96 rounded-md mb-4"></div>
+            <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-5">
+                <SkeletonCardLinkProject />
+                <SkeletonCardLinkProject />
+                <SkeletonCardLinkProject />
             </div>
         </div>
     );
