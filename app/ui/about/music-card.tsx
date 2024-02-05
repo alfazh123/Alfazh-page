223098e07aae5f6d465c020a804bbcca59582791
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-02-05 13:48:54 +0700

    init music card

---
+++ app/ui/about/music-card.tsx
@@ -0,0 +1,25 @@
+interface MusicCardProps {
+    music: {
+        link: string;
+    }[];
+}
+
+export default function MusicCard({ music }: MusicCardProps) {
+    return (
+        <div className="bg-slate-200 dark:bg-slate-800 rounded-md">
+            <ul className="snap-y snap-mandatory overflow-y-auto h-40 [&_li]:my-4">
+                {music.map((item, index) => (
+                    <li className="snap-center flex" key={index}>
+                        <iframe
+                            src={item.link}
+                            width="100%"
+                            height="152"
+                            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
+                            loading="lazy"
+                        ></iframe>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
