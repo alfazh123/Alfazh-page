4cd15f4fd374a35839e00b0ea714993735330cc3
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-02-08 15:05:24 +0700

    remove metadata from page temporary

--- app/about/books/page.tsx
+++ app/about/books/page.tsx
@@ -1,37 +1,11 @@
-"use client";
-
-import Book from "@/app/ui/about/books/book";
-import { useState } from "react";
-import { db } from "@/app/lib/db";
+import BooksPanel from "@/app/ui/about/books/books";
 import { Metadata } from "next";
 
 export default function Books() {
-    const [active, setActive] = useState(1);
     return (
         <div className="px-4">
             <h1>Books</h1>
-            <ul className="flex relative md:flex-row flex-col gap-4">
-                {db.books.map((book, id) => (
-                    <li
-                        key={book.id}
-                        onClick={() => setActive(id)}
-                        className={`md:relative cursor-pointer md:w-[8%] 
-                        md:[transition:width_var(--transition,200ms_ease-in)] md:hover:w-[20%]
-                        `}
-                    >
-                        <Book
-                            title={book.title}
-                            author={book.author}
-                            publishedYear={book.year}
-                            description={book.description}
-                            src={book.image_url}
-                            href={book.link}
-                            id={book.id}
-                            year={book.year}
-                        />
-                    </li>
-                ))}
-            </ul>
+            <BooksPanel />
         </div>
     );
 }
---
+++ app/ui/about/books/books.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Book from "@/app/ui/about/books/book";
+import { useState } from "react";
+import { db } from "@/app/lib/db";
+
+export default function BooksPanel() {
+    const [active, setActive] = useState(1);
+    return (
+        <ul className="flex relative md:flex-row flex-col gap-4">
+            {db.books.map((book, id) => (
+                <li
+                    key={book.id}
+                    onClick={() => setActive(id)}
+                    className={`md:relative cursor-pointer md:w-[8%] 
+                        md:[transition:width_var(--transition,200ms_ease-in)] md:hover:w-[20%]
+                        `}
+                >
+                    <Book
+                        title={book.title}
+                        author={book.author}
+                        publishedYear={book.year}
+                        description={book.description}
+                        src={book.image_url}
+                        href={book.link}
+                        id={book.id}
+                        year={book.year}
+                    />
+                </li>
+            ))}
+        </ul>
+    );
+}
