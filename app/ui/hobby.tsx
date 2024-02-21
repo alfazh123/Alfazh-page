f428fce67722eea348e883ad0ad3383bcdcfde79
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-02-21 16:40:00 +0700

    fixing hobby card

--- app/ui/hobby.tsx
+++ app/ui/hobby.tsx
@@ -4,81 +4,42 @@ import Skill from "./hobby/skill";
 
 export default function Hobby() {
     return (
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 mt-4 gap-4">
-            <div className=" h-40 w-full dark:bg-slate-600 bg-[#DFD8BC] items-center justify-center flex flex-col rounded-lg">
-                <Skill />
+        <div className="lg:grid lg:grid-cols-2 flex flex-col lg:space-y-0 space-y-4 gap-4 justify-center">
+            <div className="p-2 border-solid border-2 rounded-md border-[#381E70] dark:border-[#EAF7DF] flex flex-col">
+                <h1 className="font-bold text-2xl">Tech Stack</h1>
+                <div className="flex justify-center items-center">
+                    <Skill />
+                </div>
             </div>
-            <div className="md:relative lg:col-span-2">
-                <div className="md:absolute w-full h-40 md:block hidden">
+            <div className="flex relative sm:flex-row flex-col sm:items-center sm:justify-between p-2 border-solid border-2 rounded-md border-[#381E70] dark:border-[#EAF7DF] h-32 sm:h-full">
+                <section>
+                    <h1 className="font-bold text-2xl">Book</h1>
+                    <p className="italic">Books i&apos;ve read.</p>
+                </section>
+                <div className="sm:h-32 h-20 w-1/2 bottom-0 right-0 absolute sm:static">
                     <Image
-                        src="/hobby/bg-art.png"
+                        src="/bookcard.svg"
                         alt="Picture of the author"
-                        width={300}
-                        height={300}
-                        className="object-cover w-full h-full rounded-lg grayscale"
+                        width={250}
+                        height={250}
+                        className="sm:rounded-l-full rounded-tl-full h-full w-full object-cover"
                     />
                 </div>
-                <Link
-                    href="/art"
-                    className="md:absolute h-40 w-full dark:bg-slate-600 bg-[#DFD8BC] items-center justify-center flex flex-col rounded-lg hover:opacity-90 hover:ease-in-out hover:transition-all duration-700"
-                >
-                    <h2>Art</h2>
-                    <Image
-                        src="/hobby/art.png"
-                        alt="Picture of the author"
-                        width={300}
-                        height={300}
-                        className="w-20"
-                    />
-                </Link>
             </div>
-            <div className="md:relative lg:col-span-2">
-                <div className="h-40 w-full absolute md:block hidden">
+            <div className="flex relative sm:flex-row flex-col sm:items-center sm:justify-between p-2 border-solid border-2 rounded-md border-[#381E70] dark:border-[#EAF7DF] h-32 sm:h-full">
+                <section>
+                    <h1 className="font-bold text-2xl">Blogs</h1>
+                    <p className="italic">Blogs</p>
+                </section>
+                <div className="sm:h-32 h-20 w-1/2 bottom-0 right-0 absolute sm:static">
                     <Image
-                        src="/hobby/bg-art.png"
+                        src="/blogcard.svg"
                         alt="Picture of the author"
-                        width={300}
-                        height={300}
-                        className="object-cover w-full h-full rounded-lg grayscale"
+                        width={250}
+                        height={250}
+                        className="sm:rounded-l-full rounded-tl-full h-full w-full object-cover"
                     />
                 </div>
-                <Link
-                    href="/blog"
-                    className="dark:bg-slate-600 bg-[#DFD8BC] md:absolute items-center justify-center flex flex-col  h-40 w-full rounded-lg hover:opacity-90 hover:ease-in-out hover:transition-all duration-700"
-                >
-                    <h2>Blog</h2>
-                    <Image
-                        src="/hobby/blog.png"
-                        alt="Picture of the author"
-                        width={300}
-                        height={300}
-                        className="w-20"
-                    />
-                </Link>
-            </div>
-            <div className="md:relative">
-                <div className="h-40 w-full md:absolute md:block hidden">
-                    <Image
-                        src="/hobby/bg-art.png"
-                        alt="Picture of the author"
-                        width={300}
-                        height={300}
-                        className="object-cover w-full h-full rounded-lg grayscale"
-                    />
-                </div>
-                <Link
-                    href="/about/books"
-                    className="dark:bg-slate-600 bg-[#DFD8BC] items-center md:absolute justify-center flex flex-col h-40 w-full rounded-lg hover:opacity-90 hover:ease-in-out hover:transition-all duration-700"
-                >
-                    <h2>Books</h2>
-                    <Image
-                        src="/hobby/book.png"
-                        alt="Picture of the author"
-                        width={300}
-                        height={300}
-                        className="w-20"
-                    />
-                </Link>
             </div>
         </div>
     );
