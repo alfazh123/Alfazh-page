0566bf90d6a827c14481d8a37b7ce446e64707ab
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-03-26 22:36:18 +0700

    add secret

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,9 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.NEXTAUTH_SECRET;
 
 export const options: NextAuthOptions = {
     providers: [
