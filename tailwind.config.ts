4fb299cc67a0dee7b40070e2077d68f55277eeac
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-03-31 22:20:36 +0700

    config color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,17 @@ module.exports = {
                 },
             },
         },
+        colors: {
+            bone: "#EAF7DF",
+            purple: "#381E70",
+            slate200: "#e2e8f0",
+            slate300: "#cbd5e1",
+            slate400: "#94a3b8",
+            slate600: "#475569",
+            slate700: "#334155",
+            slate800: "#1e293b",
+            sand: "#DFD8BC",
+        },
     },
     plugins: [require("@tailwindcss/typography")],
 };
