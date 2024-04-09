d4233c9a462b8cbc020cee4f6f550fed6f1df4f6
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-04-09 21:10:16 +0700

    .

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,10 +1,10 @@
 import { ImageResponse } from "next/og";
-import React from "react";
-import { Open_Sans } from "next/font/google";
 
+// Route segment config
 export const runtime = "edge";
 
-export const alt = "Ahmd Alfazh";
+// Image metadata
+export const alt = "About Acme";
 export const size = {
     width: 1200,
     height: 630,
@@ -12,11 +12,16 @@ export const size = {
 
 export const contentType = "image/png";
 
-const openSans = Open_Sans({ subsets: ["latin"] });
-
+// Image generation
 export default async function Image() {
+    // Font
+    const interSemiBold = fetch(
+        new URL("./Inter-SemiBold.ttf", import.meta.url)
+    ).then((res) => res.arrayBuffer());
+
     return new ImageResponse(
         (
+            // ImageResponse JSX element
             <div
                 style={{
                     fontSize: 128,
@@ -28,20 +33,18 @@ export default async function Image() {
                     justifyContent: "center",
                 }}
             >
-                <div>
-                    <div>
-                        <h1>Ahmd Alfazh</h1>
-                    </div>
-                    <p>Front-end Developer from Surabaya, Indonesia.</p>
-                </div>
+                About Acme
             </div>
         ),
+        // ImageResponse options
         {
+            // For convenience, we can re-use the exported opengraph-image
+            // size config to also set the ImageResponse's width and height.
             ...size,
             fonts: [
                 {
-                    name: "Open Sans",
-                    data: Buffer.from(openSans.className),
+                    name: "Inter",
+                    data: await interSemiBold,
                     style: "normal",
                     weight: 400,
                 },
