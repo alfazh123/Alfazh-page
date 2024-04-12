6f90a5d1c6b88d44676363f0727c32d74837a223
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-04-13 00:56:08 +0700

    fix font

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Project() {
     return (
         <div className="flex flex-col space-y-20 px-4 pt-32">
-            <h1 className="font-bold text-6xl">Project</h1>
+            <h1 className="font-bold text-4xl">Project</h1>
             <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4">
                 {db.projects.map((project, index) => (
                     <div key={index}>
