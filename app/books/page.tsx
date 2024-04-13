2b8dfc4f8e63f1d539affc1a33d24d0111520027
Ahmd Mufahras Li Alfazh Assardew <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-04-13 23:40:51 +0700

    Revert "fix font"
    This reverts commit 036fba9aae412e35df906cdaf0507a961abfffec.

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function Books() {
     return (
         <div className="px-4 pt-32">
-            <h1 className="font-bold text-4xl">Books</h1>
+            <h1>Books</h1>
             <BooksPanel />
         </div>
     );
